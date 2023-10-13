b2ef82bb1983796e42945ae8dbe3341e6063c537
Rolly <rollydelosreyes729@gmail.com>
2023-10-13 15:21:07 +0800

    Hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
             <div className='nav-header'>React Portfolio</div>
           </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"><FaHamburger/></span>
+            <FaHamburger/>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mb-2 mb-lg-0 ms-auto">
